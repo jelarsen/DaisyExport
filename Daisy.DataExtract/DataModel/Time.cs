a7a41cdcd5fe21689d74086a2474013968bb2874
Jeremiah Larsen <jelarsen@cmkts.com>
2019-12-16 16:12:25 -0800

    Extra spacing to make it easier to read

--- Daisy.DataExtract/DataModel/Time.cs
+++ Daisy.DataExtract/DataModel/Time.cs
@@ -11,19 +11,27 @@ namespace Daisy.DataExtract.DataModel
     {
         [Name("Date")]
         public DateTime Date { get; set; }
+
         [Name("Calendar Week")]
         public string CalendarWeek { get; set; }
+
         [Name("Promo Week")]
         public string PromoWeek { get; set; }
+
         [Name("Promo Period")]
         public string PromoPeriod { get; set; }
+
         [Name("Fiscal Week")]
         public string FiscalWeek { get; set; }
+
         [Name("Fiscal Period")]
         public string FiscalPeriod { get; set; }
+
         [Name("Fiscal Quarter")]
         public string FiscalQuarter { get; set; }
+
         [Name("Fiscal Year")]
         public int FiscalYear { get; set; }
+
     }
 }
